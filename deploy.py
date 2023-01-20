8703919a2e666018df24e320c1515a4efa471439
obed <mobednfandikup@gmail.com>
2023-01-20 07:48:18 +0000

    conflict2 resolved

--- deploy.py
+++ deploy.py
@@ -1,3 +1,8 @@
 # thisis a python web application server
 print ("hello world")
 Print ("it is a new day to make new changes")
+<<<<<<< HEAD
+=======
+deploy to staging environment
+ensure security in the deployment process
+>>>>>>> 859c4ae (ensure security as you deploy)
